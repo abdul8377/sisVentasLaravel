abfad396538ec1d2a87eb71959a43222241b6414
abdul8377 <abdulalfonso77@gmail.com>
2024-06-25 21:11:15 -0500

    error wire ui

--- resources/views/livewire/product-main.blade.php
+++ resources/views/livewire/product-main.blade.php
@@ -1,7 +1,7 @@
 <div class="py-5">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-600 leading-tight">
-            Productos
+            Plantel
         </h2>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -9,7 +9,7 @@
             <x-input icon="search" placeholder="Buscar registro" wire:model.live="search"/>
             <x-button wire:click="create()" spinner="create" icon="plus" primary label="Nuevo"/>
                 @if($isOpen)
-                    @include('livewire.product-create')
+                    @include('livewire.manager-create')
                 @endif
         </div>
 
@@ -19,27 +19,27 @@
               <thead class="bg-indigo-500 text-white">
                 <tr class="text-left text-xs font-bold  uppercase">
                   <td scope="col" class="px-6 py-3">ID</td>
-                  <td scope="col" class="px-6 py-3">nombre</td>
-                  <td scope="col" class="px-6 py-3">descripcion</td>
-                  <td scope="col" class="px-6 py-3">precio</td>
-                  <td scope="col" class="px-6 py-3">stock</td>
-                  <td scope="col" class="px-6 py-3">categoria</td>
+                  <td scope="col" class="px-6 py-3">Nombre completo</td>
+                  <td scope="col" class="px-6 py-3">Cargo</td>
+                  <td scope="col" class="px-6 py-3">DNI</td>
+                  <td scope="col" class="px-6 py-3">F. Nacimiento</td>
+                  <td scope="col" class="px-6 py-3">Periodo</td>
                   <td scope="col" class="px-6 py-3 text-center">Opciones</td>
                 </tr>
               </thead>
               <tbody class="divide-y divide-gray-200 dark:text-gray-400">
-                @foreach($products as $item)
+                @foreach($managers as $item)
                 <tr class="text-sm font-medium text-gray-900">
                   <td class="px-6 py-4">
                     <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-indigo-500 text-white">
                       {{$item->id}}
                     </span>
                   </td>
-                  <td class="px-6 py-4 dark:text-gray-200">{{$item->name}}</td>
-                  <td class="px-6 py-4 dark:text-gray-200">{{$item->description}}</td>
-                  <td class="px-6 py-4 dark:text-gray-200">{{$item->price}}</td>
-                  <td class="px-6 py-4 dark:text-gray-200">{{$item->stock}}</td>
-                  <td class="px-6 py-4 dark:text-gray-200">{{$item->category}}</td>
+                  <td class="px-6 py-4 dark:text-gray-200">{{$item->fullname}}</td>
+                  <td class="px-6 py-4 dark:text-gray-200">{{$item->position}}</td>
+                  <td class="px-6 py-4 dark:text-gray-200">{{$item->dni}}</td>
+                  <td class="px-6 py-4 dark:text-gray-200">{{$item->birthdate}}</td>
+                  <td class="px-6 py-4 dark:text-gray-200">{{$item->season}}</td>
                   <td class="px-6 py-4 text-right">
                     <x-button.circle wire:click="edit({{$item}})" primary icon="pencil" />
                     <x-button.circle negative icon="x" x-on:confirm="{
@@ -55,12 +55,12 @@
               </tbody>
             </table>
         </div>
-        @if(!$products->count())
+        @if(!$managers->count())
             <p class="dark:text-gray-200">No existe ningun registro conincidente</p>
         @endif
-        @if($products->hasPages())
+        @if($managers->hasPages())
         <div class="px-6 py-3">
-            {{$products->links()}}
+            {{$managers->links()}}
         </div>
         @endif
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,20 @@
-const Color = require('color')
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-const defaultTheme = require('tailwindcss/defaultTheme')
-const plugin = require('tailwindcss/plugin')
-
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require('./vendor/wireui/wireui/tailwind.config.js')
+    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
+        './vendor/laravel/jetstream/*/.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        './src/**/*.jsx',
-        './vendor/wireui/wireui/resources/**/*.blade.php',
-        './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/src/View/**/*.php'
-
+        './resources/views/*/.blade.php',
+        './vendor/wireui/wireui/resources/*/.blade.php',
+        './vendor/wireui/wireui/ts/*/.ts',
+        './vendor/wireui/wireui/src/View/*/.php'
     ],
 
     theme: {
@@ -27,11 +22,8 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-
         },
     },
 
     plugins: [forms, typography],
 };
-
-
